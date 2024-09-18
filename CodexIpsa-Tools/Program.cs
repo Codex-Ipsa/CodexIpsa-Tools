74bbf6df57f6c2698fb364e08276cce430abf8ff
DEJVOSS <63589160+IronBeagle2@users.noreply.github.com>
2024-09-18 20:21:45 +0200

    import code

--- CodexIpsa-Tools/Program.cs
+++ CodexIpsa-Tools/Program.cs
@@ -8,8 +8,18 @@ namespace CodexIpsa_Tools
 {
     internal class Program
     {
+        public static String version = "0.1.0-wip";
         static void Main(string[] args)
         {
+            Console.Title = version;
+            Console.WriteLine(version);
+            Console.WriteLine();
+            Console.WriteLine("1) Generate VersionJsons from Mojang");
+            Console.WriteLine("2) Convert MinecraftEdu builds");
+            //Console.Write("> ");
+
+            //String selection = Console.ReadLine();
+            EduTool.start("");
         }
     }
 }
---
+++ CodexIpsa-Tools/tools/EduTool.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodexIpsa_Tools.tools
+{
+    internal class EduTool
+    {
+        public static void start(String jarPath)
+        {
+
+        }
+    }
+}
